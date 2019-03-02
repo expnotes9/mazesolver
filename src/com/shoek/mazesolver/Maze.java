3b3c9c9e9e7fac41ceb180aef93a9e4743b2a166
Shusuke Kusao <m4entacenti1@gmail.com>
2019-03-02 12:30:28 +0900

    Add files via upload

--- src/com/shoek/mazesolver/Maze.java
+++ src/com/shoek/mazesolver/Maze.java
@@ -26,11 +26,10 @@ package com.shoek.mazesolver;
 
 import java.io.*;
 import java.util.*;
+import java.util.function.BiConsumer;
 
 public class Maze {
 
-    public static final int MAX_HEIGHT = 2001;
-    public static final int MAX_WIDTH = 2001;
     public static final int MAX_SIMPLICITY = 100;
     public static final char WALL = '#';
     public static final char ROAD = '.';
@@ -41,6 +40,22 @@ public class Maze {
     private static final int INF = Integer.MAX_VALUE;
 
     public static void main(String args[]) {
+        // 標準入力例
+        // 迷路作る
+        // 11 13 0 0 -make -debug
+        // 作って解く
+        // 5 15 0 0 -bfs -debug
+        // 一本道マシマシ
+        // 5 15 100 0 -bfs -debug
+        // 回廊がないじゃん->適当に壁を掘る
+        // 5 21 0 20 -bfs -debug
+        // 最短経路でなく到達可能か調べるやつ
+        // 5 21 0 20 -dfs -debug
+
+        // 上限調査用
+        // 2501 2501 50 1000 -make -no-debug
+        // 2501 2501 50 1000 -bfs -no-debug
+        // 2501 2501 50 1000 -dfs -no-debug
 
         Scanner scn = new Scanner(System.in);
         PrintWriter cout = new PrintWriter(System.out);
@@ -49,22 +64,38 @@ public class Maze {
         int width = scn.nextInt();
         int simplicity = scn.nextInt();
         int circuits = scn.nextInt();
-        boolean debug = Objects.equals("-d", scn.next());
+        String proc = scn.next();
+        boolean debug = Objects.equals("-debug", scn.next());
 
+        BiConsumer<int[], ArrayDeque<int[]>> addDeq = null;
+        if (Objects.equals(proc, "-bfs")) {
+            addDeq = ADD_BFS;
+        } else if (Objects.equals(proc, "-dfs")) {
+            addDeq = ADD_DFS;
+        } else {
+            // makeのみ。
+        }
+
+        // make:迷路を作ります
         char[][] maze = make(height, width, simplicity, circuits);
-
         if (debug) {
             cout.println("make:");
             for (char[] line : maze)
                 cout.println(String.valueOf(line));
         }
 
-        shortestPath(maze);
+        if (addDeq == null) {
+            // makeのみ。
+        } else {
 
-        if (debug) {
-            cout.println("shortestPath:");
-            for (char[] line : maze)
-                cout.println(String.valueOf(line));
+            // bfs/dfs:迷路を解きます
+            xfs(maze, addDeq);
+            if (debug) {
+                cout.println(proc + ":");
+                for (char[] line : maze)
+                    cout.println(String.valueOf(line));
+            }
+
         }
 
         cout.flush();
@@ -74,12 +105,12 @@ public class Maze {
         final int h = height; // 縦幅
         final int w = width; // 横幅
 
-        if (3 > h || h > MAX_HEIGHT || h % 2 == 0) {
-            System.out.println("3 <= height <= " + MAX_HEIGHT + " の奇数のみ入力可能です");
+        if (3 > h || h % 2 == 0) {
+            System.out.println("3 <= height の奇数のみ入力可能です");
             return new char[0][0];
         }
-        if (3 > w || w > MAX_WIDTH || w % 2 == 0) {
-            System.out.println("3 <= width <= " + MAX_WIDTH + " の奇数のみ入力可能です");
+        if (3 > w || w % 2 == 0) {
+            System.out.println("3 <= width の奇数のみ入力可能です");
             return new char[0][0];
         }
         if (h * w == 9) {
@@ -98,6 +129,17 @@ public class Maze {
         // 戻り値（迷路）
         char[][] maze = new char[h][w];
 
+        // （１）壁で埋め尽くします
+        for (char[] line : maze)
+            Arrays.fill(line, WALL);
+        // 外周は一時的に専用文字に書き換えます（番兵法）
+        Arrays.fill(maze[0], TEMP_STOPPER);
+        Arrays.fill(maze[h - 1], TEMP_STOPPER);
+        for (int i = 0; i < h; i++) {
+            maze[i][0] = TEMP_STOPPER;
+            maze[i][w - 1] = TEMP_STOPPER;
+        }
+
         // 上下左右
         int[] dy = { 0, -1, 0, 1 };
         int[] dx = { -1, 0, 1, 0 };
@@ -106,30 +148,18 @@ public class Maze {
         // 乱数
         Random rand = new Random(new Date().getTime());
 
-        // （１）壁で埋め尽くします
-        for (char[] line : maze)
-            Arrays.fill(line, WALL);
-        Arrays.fill(maze[0], TEMP_STOPPER);
-        Arrays.fill(maze[h - 1], TEMP_STOPPER);
-        for (int i = 0; i < h; i++) {
-            maze[i][0] = TEMP_STOPPER;
-            maze[i][w - 1] = TEMP_STOPPER;
-        }
-
-        // 掘る開始地点リスト
-        // （初期サイズh*w/32は適当。最大サイズ < h*w/4なのは間違いない）
-        PriorityQueue<int[]> q = new PriorityQueue<>(Math.max(1, h * w / 32),
-                (arr1, arr2) -> Integer.compare(arr1[2], arr2[2]));
+        // 到達済み地点リスト
+        PriorityQueue<int[]> q = new PriorityQueue<>((arr1, arr2) -> Integer.compare(arr1[2], arr2[2]));
 
         // （２）真ん中辺りの奇数マスに道を開けます
         int[] stt = { h / 4 * 2 + 1, w / 4 * 2 + 1, 0 };
         maze[stt[0]][stt[1]] = ROAD;
 
-        // （３）真ん中を掘る開始地点リストに追加します
+        // （３）真ん中を到達済地点リストに追加します
         q.add(stt);
         while (q.isEmpty() == false) {
 
-            // （４）掘る開始地点リストの先頭を取得します
+            // （４）到達済地点リストの先頭を取り出します
             int[] p = q.poll();
             int y = p[0];
             int x = p[1];
@@ -151,35 +181,36 @@ public class Maze {
                     // 別の経路で既に掘ってます
                     continue;
 
-                // よし掘れる、掘ろう
+                // （６）よし掘れる、掘ろう
                 maze[ny1][nx1] = ROAD;
                 maze[ny2][nx2] = ROAD;
 
-                // 厳密に言うと「掘る開始地点リスト」はリストでなく優先度キューです
-                // 中身が常時ソートされた状態になっており、指定した順番で処理するのに効率がいいです
+                // （７）simplicityパーセントで続きを掘ります
+                // (100 - simplicity)パーセントでランダム順に掘る予約をします
                 int priority;
-                if (rand.nextInt(MAX_SIMPLICITY) < simplicity) {
-                    // 今回に掘った道の続きを最優先で掘ります
+                if (simplicity == 0) {
+                    priority = rand.nextInt(INF);
+                } else if (simplicity == MAX_SIMPLICITY) {
+                    priority = -1;
+                } else if (rand.nextInt(MAX_SIMPLICITY) < simplicity) {
                     priority = -1;
                 } else {
-                    // p[2]の小さいやつから処理します
-                    // ＝今までに掘った道から適当に選んで次の道を掘ります
                     priority = rand.nextInt(INF);
                 }
 
-                // 掘った先を掘る開始地点リストに追加します
+                // （７）掘った先を到達済地点リストに追加します
                 q.add(new int[] { ny2, nx2, priority });
+
                 // 現在地点もリストに戻します
-                // （１方向に掘ったら他の方向を掘らずに終わるので、戻しておかないと掘り漏れが発生します。
-                // 全方向を掘り終わっていたら単に空振りします）
+                // （１方向に掘ったら他の方向を掘らずに終わるので、戻しておかないと掘り漏れが発生します。全方向を掘り終わっていたら単に空振りします）
                 q.add(new int[] { p[0], p[1], rand.nextInt(INF) });
                 // 他の方向は掘らずに終わります
                 break;
             }
         }
-        // （６）掘る開始地点リストが空になったら終わりです、お疲れ様
+        // （８）到達済地点リストが空になったら掘り終わりです
 
-        // （７）仕上げ１：外周をただの壁に戻します
+        // （９）仕上げ１：外周をただの壁に戻します
         Arrays.fill(maze[0], WALL);
         Arrays.fill(maze[h - 1], WALL);
         for (int i = 0; i < h; i++) {
@@ -202,7 +233,7 @@ public class Maze {
             }
         }
 
-        // （８）仕上げ２：適当な道に開始地点と終了地点を置きます
+        // （１０）仕上げ２：適当な道に開始地点と終了地点を置きます
         int startPoint = rand.nextInt(roadsCount);
         int endPoint = rand.nextInt(roadsCount);
         while (startPoint == endPoint)
@@ -221,8 +252,10 @@ public class Maze {
             }
         }
 
-        // （９）仕上げ３：適当な壁に道を開けます
-        // （作成直後の迷路には閉路＝回り道がありません）
+        // （１１）仕上げ３：circuitsの指定だけ、適当に壁を掘ります
+        // 作成直後の迷路はグラフ理論で言う木です。閉路＝回り道がありません。
+        // そのままだと退屈なので適当に回り道を作ります
+        // 重複ありです。入力とリアルラック次第では同じ位置を２回掘ろうとします
         Set<Integer> c = new HashSet<>(circuits);
         for (int i = 0; i < circuits; i++)
             c.add(rand.nextInt(wallsCount));
@@ -242,7 +275,11 @@ public class Maze {
         return maze;
     }
 
-    public static void shortestPath(char[][] maze) {
+    private static BiConsumer<int[], ArrayDeque<int[]>> ADD_BFS = (point, deq) -> deq.addFirst(point);
+
+    private static BiConsumer<int[], ArrayDeque<int[]>> ADD_DFS = (point, deq) -> deq.addLast(point);
+
+    public static void xfs(char[][] maze, BiConsumer<int[], ArrayDeque<int[]>> addDeq) {
         // 最短経路を探してmazeを書き換えます（破壊的動作）
 
         final int h = maze.length; // 縦幅
@@ -284,30 +321,25 @@ public class Maze {
             throw new IllegalArgumentException("開始地点" + START + "または終了地点" + GOAL + "がありません");
         }
 
-        // （２）開始地点をキューに入れます
-
-        // アルゴリズムの世界でキューとはFIFO（先入先出法）を指し、
-        // それ以外はキューと呼びません
-        // つまり出す時はq.pollFirst()の位置のデータを出し、
-        // 入れる時はq.addLast()の位置に入れます
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.add(s);
+        // （２）開始地点をコレクション（キューまたはスタック）に入れます
+        ArrayDeque<int[]> deq = new ArrayDeque<>();
+        deq.add(s);
         vis[s[0]][s[1]] = 0;
 
-        outer: while (q.isEmpty() == false) {
+        while (deq.isEmpty() == false) {
 
-            // （３）キューから取得します
-            int[] p = q.pollFirst();
+            // （３）コレクションから取り出します
+            int[] p = deq.pollLast();
             int y = p[0];
             int x = p[1];
             int m = p[2];
             vis[y][x] = m;
             if (y == g[0] && x == g[1]) {
                 // （４）ゴールしてたら終了。
-                break outer;
+                break;
             }
 
-            // （５）分身の術！
+            // （５）上下左右に移動してみます
             for (int d = 0; d < dn; d++) {
                 // 隣のマスが
                 int ny = y + dy[d];
@@ -317,11 +349,18 @@ public class Maze {
                     continue;
                 // 未計算の場合＝行ったことない道かゴールの場合
                 int[] np = new int[] { ny, nx, 1 + m };
-                // （６）ここに分身を置いておこう
-                q.addLast(np);
+                // （６）分身の術！
+                // コレクションに分身を入れておきます
+                // 前述の取り出しがpollLastであることに注意。
+                // BFSは取り出し時と逆に入れます（FIFO：先入先出法）
+                // DFSは取り出し時と同じに入れます（LIFO：先入先出法）
+                addDeq.accept(np, deq);
             }
         }
-        q.clear();
+        deq.clear();
+        if (vis[g[0]][g[1]] == -1) {
+            throw new IllegalArgumentException("終了地点に到達できませんでした");
+        }
 
         // 最短距離をmazeに書き込みましょう
 
@@ -346,7 +385,7 @@ public class Maze {
 
         // 終了地点をマークに戻して
         maze[g[0]][g[1]] = GOAL;
-        // お疲れ様
+        // 〜終わり〜
     }
 
 }
